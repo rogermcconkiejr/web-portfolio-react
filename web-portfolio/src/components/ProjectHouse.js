604d59bb60d134895589dcfefd7ecf8755be39d4
Roger McConkie <roger.mcconkiejr@gmail.com>
2020-01-10 10:49:36 -0700

    projects are now included and have links

--- web-portfolio/src/components/ProjectHouse.js
+++ web-portfolio/src/components/ProjectHouse.js
@@ -2,6 +2,9 @@ import React from 'react';
 import designHub from "../Assets/designHub.png"
 import tipsease from "../Assets/tipsease.png"
 import diy from "../Assets/diy.png"
+import nasa from "../Assets/nasa.png"
+import guidr from "../Assets/guidr.png"
+import githubcard from "../Assets/githubcard.png"
 
 function ProjectHouse() {
     return (
@@ -9,20 +12,30 @@ function ProjectHouse() {
             <div className="portTitle">Portfolio</div>
             
             <div className="pjHouse">
-                <div className="pjImgHouse">
+                <a href="https://designhubx.com/explore"><div className="pjImgHouse">
                 <img className="pjPic" src={designHub} alt="Design Hub"/>
                 <div className="pjTitle">"Design Hub"</div>
-                </div>
-                <div className="pjImgHouse">
+                </div></a>
+                <a href="https://tipseaseui-roger.rogermcconkiejr.now.sh/"><div className="pjImgHouse">
                 <img className="pjPic" src={tipsease} alt="Tipsease"/>
                 <div className="pjTitle">"Tipsease"</div>
-                </div>
-                <div className="pjImgHouse">
+                </div></a>
+                <a href="https://diy-tracker.rogermcconkiejr.now.sh/login"><div className="pjImgHouse">
                 <img className="pjPic" src={diy} alt="DIY Project"/>
                 <div className="pjTitle">"DIY Tracker"</div>
-                </div>
-
-
+                </div></a>
+                <a href="https://nasa-photo-of-the-day.rogermcconkiejr.now.sh/"><div className="pjImgHouse">
+                <img className="pjPic" src={nasa} alt="NASA photo of the day project"/>
+                <div className="pjTitle">"NASA photo of the day"</div>
+                </div></a>
+                <a href="https://guidr.now.sh/"><div className="pjImgHouse">
+                <img className="pjPic" src={guidr} alt="GUIDR Project"/>
+                <div className="pjTitle">"Guidr"</div>
+                </div></a>
+                <a href="https://githubcard.now.sh/"><div className="pjImgHouse">
+                <img className="pjPic" src={githubcard} alt="Git Hub Followers Project"/>
+                <div className="pjTitle">"Git Hub Followers"</div>
+                </div></a>
             </div>
             <hr /> 
         </div>
